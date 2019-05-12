14de4d6f3c01ebed74f88e1967c0bdda0dc61feb
Eddort <webisforce@gmail.com>
2019-05-12 16:13:58 +0300

    styled wrapper

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('init')
+import { makeRulesWithEffect } from './styles';
+
+const makeElementary = styled => (tag, styles, config) =>
+  styled(tag)(makeRulesWithEffect(styles, config));
+
+const witchStyled = styled => {
+  const elementary = makeElementary(styled);
+  return Object.keys(styled).reduce((acc, tag) => {
+    acc[tag] = elementary.bind(null, tag);
+    return acc;
+  }, elementary);
+};
+
+export default witchStyled;
