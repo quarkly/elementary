55fe7f5b517a4a0a669216dfbda30a492beffc03
Eddort <webisforce@gmail.com>
2019-05-12 13:34:16 +0300

    base rule logic

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,12 @@
-import { isArray, map } from 'lodash/fp';
+import { isArray, map, isNumber, isUndefined, get, entries, flow } from 'lodash/fp';
 import PropTypes from 'prop-types';
+import stylesDict from './dict';
+
+export const defaultBreakpoints = [40, 52, 64].map(n => `${n}em`);
+
+export const stylesDictValues = Object.values(stylesDict);
+
+export const getByAlias = alias => stylesDictValues.find(prop => prop.alias === alias);
 
 export const typeToPropTypes = type => PropTypes[type];
 
@@ -8,4 +15,49 @@ export const toPropTypes = style =>
     ? PropTypes.oneOfType(map(typeToPropTypes, style.type))
     : typeToPropTypes(style.type);
 
-export default '';
+export const toPx = n => (isNumber(n) && n !== 0 ? `${n}px` : n);
+
+export const createMediaQuery = n => `@media screen and (min-width: ${toPx(n)})`;
+
+export const transformers = {};
+
+export const getTransformer = name => transformers[name] || (value => value);
+
+export const makeRule = (property /* config */) => {
+  const result = [null, null];
+  // Инициализация - старт
+  if (!stylesDict[property]) {
+    return result;
+  }
+  const { alias, transformerName } = stylesDict[property];
+  const transform = getTransformer(transformerName);
+  const propertyName = alias || property;
+  // Инициализация - конец
+  const rule = props => {
+    let resultRule;
+    // ищем свойство в пропсах
+    const propertyValue = get(propertyName, props);
+    if (isUndefined(propertyValue)) {
+      return resultRule;
+    }
+    const createStyle = n => ({
+      [property]: transform(n),
+    });
+    if (isArray(propertyValue)) {
+      const breakpoints = get('breakpoints', props.theme) || defaultBreakpoints;
+      // количество свойств в массиве должно быть не больше чем в брейкпоинтах
+      const consistentValues = propertyValue.slice(0, breakpoints.length + 1);
+      resultRule = flow(
+        entries,
+        map(([key, value]) => ({
+          [createMediaQuery(key)]: createStyle(value),
+        })),
+      )(consistentValues);
+    } else {
+      resultRule = createStyle(propertyValue);
+    }
+    return resultRule;
+  };
+  const propType = toPropTypes(stylesDict[property]);
+  return [rule, propType];
+};
--- src/styles/styles.test.js
+++ src/styles/styles.test.js
@@ -1,5 +1,5 @@
 import dict from './dict';
-import { toPropTypes } from './index';
+import { toPropTypes, makeRule } from './index';
 
 describe('Style dict', () => {
   test('type exists in all style object', () => {
@@ -7,5 +7,20 @@ describe('Style dict', () => {
   });
   test('to propTypes convert', () => {
     Object.values(dict).forEach(style => expect(toPropTypes(style)).toBeDefined());
+  });
+});
+describe('Create rule', () => {
+  test('base', () => {
+    Object.keys(dict).forEach(style => expect(makeRule(style)).toBeDefined());
+  });
+});
+describe('Use rule', () => {
+  test('base', () => {
+    const [rule] = makeRule('backgroundColor');
+    expect(rule({ bg: 'red' })).toStrictEqual({ backgroundColor: 'red' });
+    expect(rule({ bg: ['red', 'green'] })).toStrictEqual([
+      { '@media screen and (min-width: 0)': { backgroundColor: 'red' } },
+      { '@media screen and (min-width: 1)': { backgroundColor: 'green' } },
+    ]);
   });
 });
