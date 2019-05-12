0c25895a91a64ef5e9cae61b72737960af42cfe9
Eddort <webisforce@gmail.com>
2019-05-12 18:58:47 +0300

    integration test for sc

--- test/sc.test.js
+++ test/sc.test.js
@@ -20,6 +20,9 @@ const el = elementary(styled);
 
 const [Box] = el('div')(['color'], { effects: { hover: ':hover' } });
 const [ComposedBox] = el('div')(['color'], { effects: { hover: ':hover', focus: ':focus' } });
+const [ChainBox] = el.div(['color'], { effects: { hover: ':hover', focus: ':focus' } });
+const [ExtendedSC] = el(Button)(['color'], { effects: { hover: ':hover' } });
+const [ExtendedEL] = el(Box)(['margin'], { effects: { hover: ':hover' } });
 
 describe('Elementary integration to SC', () => {
   test('base', () => {
@@ -61,5 +64,22 @@ describe('Elementary integration to SC', () => {
       media: 'screen and (min-width: 40em)',
       modifier: ':hover',
     });
+    expect(tree).toHaveStyleRule('color', 'green', {
+      media: 'screen and (min-width: 52em)',
+      modifier: ':hover',
+    });
+  });
+  test('chain call from elemetary', () => {
+    const tree = renderer.create(<ChainBox hoverColor="red" color="blue" />).toJSON();
+    expect(tree).toHaveStyleRule('color', 'blue');
+  });
+  test('extend from sc component', () => {
+    const tree = renderer.create(<ExtendedSC hoverColor="red" color="blue" />).toJSON();
+    expect(tree).toHaveStyleRule('color', 'blue');
+  });
+  test('extend from el component', () => {
+    const tree = renderer.create(<ExtendedEL hoverColor="red" color="blue" m="100px" />).toJSON();
+    expect(tree).toHaveStyleRule('color', 'blue');
+    expect(tree).toHaveStyleRule('margin', '100px');
   });
 });
