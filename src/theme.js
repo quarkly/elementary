9cf79033f2cbf33302979896d3d135e76d10e7c8
Eddort <webisforce@gmail.com>
2019-05-12 20:58:51 +0300

    edit readme

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const themeDefault = {
+  breakpoints: ['576px', '768px', '992px', '1200px'],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  colors: {
+    blue: '#07c',
+    lightgray: '#f6f6ff',
+    primary: '#2973B7',
+    effect: '#248DE0',
+  },
+  space: [0, 4, 8, 16, 32, 64, 128, 256],
+};
+
+export default themeDefault;
